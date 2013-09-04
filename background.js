b4c3ee12f229dc5f6fe8c1d95aeaabaabafd771b
moyashipan <moyashipan@gmail.com>
2013-09-04 20:44:30 +0900

    主要APIへのリクエストをフック

---
+++ background.js
@@ -0,0 +1,62 @@
+var members = [];
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details){
+		switch (details.url) {
+		case "https://www.facetouch.jp/webApi/frontLogin":
+			parseFrontLogin(details);
+			break;
+		case "https://www.facetouch.jp/webApi/getMemberDetail":
+			parseGetMemberDetail(details);
+			break;
+		case "https://www.facetouch.jp/webApi/sendMail":
+			parseSendMail(details);
+			break;
+		}
+		return details;
+	},
+	{
+		urls: [
+			"https://www.facetouch.jp/webApi/frontLogin",
+			"https://www.facetouch.jp/webApi/getMemberDetail",
+			"https://www.facetouch.jp/webApi/sendMail"
+		]
+	},
+	[
+		"requestBody"
+	]
+);
+
+function parseFrontLogin(details) {
+	// 2回目のリクエストであれば無視
+	if (details.requestBody.formData.second) return;
+
+	// メンバ一覧が欲しいので2回目のリクエストであるフラグを追加して再度postする
+	var formData = details.requestBody.formData;
+	for (var i in formData) {
+		formData[i] = formData[i].join("");
+	}
+	formData.second = '1';
+
+	$.post(
+		details.url,
+		formData,
+		function(data){
+			$.each(data.members, function(){
+				members[this.user_id] = this;
+			});
+		}
+	);
+}
+
+function parseGetMemberDetail(details) {
+}
+
+function parseSendMail(details) {
+	var user_id = details.requestBody.formData.user_id.join('');
+	var user = members[user_id];
+	if (!user) return;
+
+	// TODO: Post somewhere!
+}
+
